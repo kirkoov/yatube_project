bbf37d0623fb6baa8d6f6c0d5569a8a281df599d
Кирилл Кувшинов <k.koovshinov@gmail.com>
2023-02-07 07:09:12 +0300

    Push before the Ya.P test db.sqlite3 is used.

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -8,12 +8,12 @@ from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
     # Перечисляем поля, которые должны отображаться в админке
-    list_display = ('pk', 'text', 'pub_date', 'author')
+    list_display = ('pk', 'text', 'pub_date', 'author',)
     # Добавляем интерфейс для поиска по тексту постов
     search_fields = ('text',)
     # Добавляем возможность фильтрации по дате
     list_filter = ('pub_date',)
-    empty_value_display = '-пусто-'
+    # empty_value_display = '-пусто-'
 
 
 # При регистрации модели Post источником конфигурации для неё назначаем
